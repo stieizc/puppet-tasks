667ebfc5c012ae0b42becfa86bd770a108b13f93
Wenxin Wang <wenxin.wang94@gmail.com>
2016-02-25 11:34:31 +0800

    source system bash aliases

--- local-modules/app/manifests/bash.pp
+++ local-modules/app/manifests/bash.pp
@@ -2,12 +2,24 @@
 class app::bash {
   $user_name = hiera('user_name')
   $user_home = "/home/${user_name}"
+  if $::osfamily == 'Debian' {
+    $sys_bashrc = '/etc/bash.bashrc'
+  }
+  else {
+    $sys_bashrc = '/etc/bashrc'
+  }
 
   file { '/etc/bash.aliases':
     ensure => present,
     source => 'puppet:///modules/app/bash/bash.aliases',
     owner  => root,
     group  => root,
+  }
+
+  file_line { 'source system bash aliases':
+    ensure => present,
+    path   => $sys_bashrc,
+    line   => '[[ -r /etc/bash.aliases ]] && . /etc/bash.aliases',
   }
 
   file { "${user_home}/.bashrc":
